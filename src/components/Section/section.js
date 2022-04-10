c2bbacfc498e589e9705889e01c03009c79e3c79
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2022-04-11 00:21:43 +0300

    cnbkb

--- src/components/Section/section.js
+++ src/components/Section/section.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Section.css';
 
 const Section = ({ title, children }) => {
   return (
     <section className={styles.section}>
-      <h1 className={styles.title}>{title}</h1>
+      <h2 className={styles.title}>{title}</h2>
       {children}
     </section>
   );
@@ -12,5 +13,6 @@ const Section = ({ title, children }) => {
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
 };
 export default Section;
